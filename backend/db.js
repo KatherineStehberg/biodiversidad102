9871bcb4d61262c0069eaf0af18025efac026f28
Katherine Stehberg <kstehberg@gmail.com>
2024-09-30 16:01:13 -0300

    db

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,17 @@
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
-// Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
-// Crear una nueva instancia de Pool para la conexión con PostgreSQL
+// Use the connection string directly
 const pool = new Pool({
-  user: process.env.DB_USER,        // Usuario de la base de datos
-  host: process.env.DB_HOST,        // Host de la base de datos (desde variable de entorno)
-  database: process.env.DB_NAME,    // Nombre de la base de datos (desde variable de entorno)
-  password: process.env.DB_PASSWORD,// Contraseña de la base de datos
-  port: process.env.PGPORT || 5432, // Puerto de PostgreSQL
-  ssl: process.env.DB_URL.includes("render") ? { rejectUnauthorized: false } : false // Solo usar SSL si la URL contiene "render"
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false // Required for Render connections
+  }
 });
 
-// Verificar la conexión
+// Verifying connection
 pool.connect((err) => {
   if (err) {
     console.error('Error al conectar con la base de datos', err);
@@ -24,3 +21,4 @@ pool.connect((err) => {
 });
 
 module.exports = pool;
+
